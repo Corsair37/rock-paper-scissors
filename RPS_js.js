b5d344fe8097e5dfbaad50f14e137e29267553c6
Corsair <999.ala37@gmail.com>
2024-03-17 10:50:06 -0400

    Add functions
    Very big jump from having one smaller function to the entire project done, but here it is. Essentially, there are three functions: one for the player, one for the computer, and one for the actual game itself. The player function gives the user a prompt to type rock, paper, or scissors and parse it into lowercase. The computer function generates a number between 1 and 3, then chooses either Rock, Paper, or Scissors accordingly. The game function compares the results of the two previous functions and has six outcomes.

--- RPS_js.js
+++ RPS_js.js
@@ -1,3 +1,15 @@
+let playerChoice = prompt("Rock, Paper, or Scissors?")
+function getPlayerChoice (playerChoice) {
+    let shortstack = playerChoice.toLowerCase();
+    if (shortstack = "rock") {
+        return "rock"
+    } else if (shortstack = "paper") {
+        return "paper"
+    } else if (shortstack = "scissors"){
+        return "scissors"
+    }
+}
+
 function getComputerChoice () {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 1) {
@@ -10,4 +22,22 @@ function getComputerChoice () {
         
 }
 
-console.log(getComputerChoice())
+let getPlayerChoice = getPlayerChoice ();
+let getComputerChoice = getComputerChoice ();
+
+function playRound (getComputerChoice,getPlayerChoice) {
+    if (getplayerChoice === "rock" && getComputerChoice === "Paper") {
+        return "Nice try, goomba, but paper beats rock."
+    } else if (getPlayerChoice === "rock" && getComputerChoice === "Scissors") {
+        return "Good stuff, choom. Rock beats scissors."
+    } else if (getPlayerChoice === "paper" && getComputerChoice === "Rock") {
+        return "Preem, choom. Paper beats rock."
+    } else if (getPlayerChoice === "paper" && getComputerChoice === "Scissors") {
+        return "Damn. Nice try, but scissors beats paper."
+    } else if (getPlayerChoice === "scissors" && getComputerChoice === "Rock") {
+        return "Rest in pizza. Rock beats paper, choom."
+    } else if (getPlayerChoice === "scissors" && getComputerChoice === "Paper") {
+        return "Nice, choom. Scissors beat Paper"
+    }
+}
+ console.log(playRound())
